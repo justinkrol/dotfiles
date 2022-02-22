e91c5280ee44832448cfb9c73b564c5446dcffa5
Justin Krol <justin.krol@shopify.com>
2022-02-22 17:11:04 +0900

    Custom aliases

--- personal/antigen_bundles.zsh
+++ personal/antigen_bundles.zsh
@@ -1,7 +1,7 @@
 # Additional oh-my-zsh plugins to include
 # Default bundles included can be seen in core/default_bundles.zsh
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Plugins for available oh-my-zsh plugins.
-# See https://github.com/zsh-users/antigen/wiki/Commands#antigen-bundle for instructions on including custom plugins. 
+# See https://github.com/zsh-users/antigen/wiki/Commands#antigen-bundle for instructions on including custom plugins.
 #
 # Include a plugin with `antigen bundle <plugin-name>`.
 
@@ -12,5 +12,4 @@
 # custom caching keys for different environments, if you desire that. The cache speeds up your terminal startup, so
 # try to avoid disabling the cache unless you have no other choice.
 
-# Staples theme: https://github.com/dersam/staples
-antigen theme dersam/staples staples
+antigen theme cloud
--- personal/custom.zsh
+++ personal/custom.zsh
@@ -2,3 +2,8 @@
 # This file is loaded after everything else, but before Antigen is applied and ~/extra.zsh sourced.
 # Put anything here that you want to exist on all your environment, and to have the highest priority
 # over any other customization.
+
+alias gl='git log --pretty=format:"%h %cd : %s" -20'
+alias gdm='git diff HEAD^ HEAD'
+alias gr='git reset'
+alias delete_branches='git branch --merged master | grep -v "\* master" | grep -v "master" | xargs -n 1 git branch -d'
